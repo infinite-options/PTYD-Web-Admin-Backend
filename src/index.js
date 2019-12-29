ddcf8fbe1d0efddc0bd39c55084d03d9e97ce439
Tangni Wang <twang63@ucsc.edu>
2019-12-28 22:00:41 -0800

    added react mdl

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+
 import "bootstrap/dist/css/bootstrap.min.css";
+import "react-mdl/extra/material.css";
+import "react-mdl/extra/material.js";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
