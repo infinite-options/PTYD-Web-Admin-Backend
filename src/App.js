d36167080f385d0fb7f40d5861f6d522fb8bceb3
Tangni Wang <twang63@ucsc.edu>
2019-12-30 17:48:25 -0800

    section 1 on homepage

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
 import Main from "./components/main";
-import { Header, Drawer, Navigation, Content } from "react-mdl";
-import Layout from "./components/layout";
+import { Link } from "react-router-dom";
 
-function App() {
-  return (
-    <div style={{ height: "300px", position: "relative" }}>
-      <Layout>
-        <Header
-          title={
-            <span>
-              <span style={{ color: "#ddd" }}>Area / </span>
-              <strong>The Title</strong>
-            </span>
-          }
-        >
-          <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-          </Navigation>
-        </Header>
-        <Drawer title="Title">
-          <Navigation>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-            <a href="/">Link</a>
-          </Navigation>
-        </Drawer>
-        <Content>
-          <Main />
-        </Content>
-      </Layout>
-    </div>
-  );
+class App extends Component {
+  static defaultProps = {
+    icon: "fas fa-home"
+  };
+
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header
+            className="header-color"
+            title={
+              <span>
+                <span style={{ color: "#ddd" }}>Area / </span>
+                <strong>The Title</strong>
+              </span>
+            }
+            scroll
+          >
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer
+            style={{ backgroundColor: "#493f3f" }}
+            className="header-color-background"
+            title={
+              <Link style={{ fontFamily: "Kalam", color: "white" }} to="/">
+                Home
+              </Link>
+            }
+          >
+            <Navigation>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+              <a href="/">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content" />
+            <Main />
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,15 +1,49 @@
 import React, { Component } from "react";
+import Profile from "../img/img1.jpg";
 
 class Landing extends Component {
   render() {
     return (
       <div>
-        <h1>
-          {" "}
-          landing pagefdsf sdfsdfsd fdeghf dgnhsk fdjgnk lsnkljg nsfdn gjlngjd
-          fsnljf dfdghd fgfdsg fdgdfgdfgdfgfdg dfgdfg dfgdfgdf fdsf fsggght
-          frdsfsdf hntrhrth tdgrdg
-        </h1>
+        <section class="content-section">
+          <h3>
+            <div class="container" style={{ color: "white" }}>
+              <img
+                class="img-fluid"
+                style={{ width: "100%", height: "100%" }}
+                src={Profile}
+                alt=""
+              />
+              <div class="top-left" style={{ size: "300px" }}>
+                LOCAL<br></br>ORGANIC<br></br>RESPONSIBLE
+              </div>
+
+              <div
+                class="bottom-left"
+                style={{ marginBottom: "200px", fontSize: "20px" }}
+              >
+                Farm-to-table, prepared meals <br></br>delivered to your
+                doorstep.<br></br>
+                <button
+                  type="button"
+                  class="btn1 btn1-primary"
+                  style={{ marginTop: "10px", color: "white" }}
+                >
+                  Get Started
+                </button>
+              </div>
+
+              <div class="bottom-left">Already a Mmeber? Sign in</div>
+            </div>
+            <div class="container">
+              <div class="langding-part2">
+                <center>
+                  <h2>SAVE TIME, EAT BETTER.</h2>
+                </center>
+              </div>
+            </div>
+          </h3>
+        </section>
       </div>
     );
   }
