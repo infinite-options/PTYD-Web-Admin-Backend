8267f1a85d7c60fc5962f4a1190ecfb435096a2d
Tangni Wang <twang63@ucsc.edu>
2019-12-31 12:09:25 -0800

    second section

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Profile from "../img/img1.jpg";
+import { Row, Col, Container } from "react-bootstrap";
 
 class Landing extends Component {
   render() {
@@ -10,17 +11,25 @@ class Landing extends Component {
             <div class="container" style={{ color: "white" }}>
               <img
                 class="img-fluid"
-                style={{ width: "100%", height: "100%" }}
+                style={{ width: "100%", height: "700px" }}
                 src={Profile}
                 alt=""
               />
-              <div class="top-left" style={{ size: "300px" }}>
+              <div
+                class="top-left"
+                style={{ size: "300px", lineHeight: "50px" }}
+              >
                 LOCAL<br></br>ORGANIC<br></br>RESPONSIBLE
               </div>
 
               <div
                 class="bottom-left"
-                style={{ marginBottom: "200px", fontSize: "20px" }}
+                style={{
+                  marginBottom: "200px",
+                  fontSize: "20px",
+                  textShadow: "2px 2px 4px #000000",
+                  lineHeight: "35px"
+                }}
               >
                 Farm-to-table, prepared meals <br></br>delivered to your
                 doorstep.<br></br>
@@ -37,9 +46,40 @@ class Landing extends Component {
             </div>
             <div class="container">
               <div class="langding-part2">
+                <br></br>
                 <center>
                   <h2>SAVE TIME, EAT BETTER.</h2>
                 </center>
+                <hr></hr>
+                <Container>
+                  <Row style={{ fontSize: "25px", color: "#196F3D" }}>
+                    <Col>CHOOSE YOUR PLAN</Col>
+                    <Col>WEEKLY DELIVERY</Col>
+                    <Col>NO COMMITMENT</Col>
+                  </Row>
+                  <Row
+                    style={{
+                      fontSize: "15px",
+                      color: "black",
+                      lineHeight: "25px"
+                    }}
+                  >
+                    <Col>
+                      Order 5, 10, or 20 meals each week. Customize your order
+                      from 6 new recipes every week.
+                    </Col>
+                    <Col>
+                      We deliver in Austin, Texas to your home or office.
+                      Delivery times are Sunday 6pm to 9pm and Monday 9am to
+                      1pm.{" "}
+                    </Col>
+                    <Col>
+                      Prep to Your Door is flexible. You can modify, skip, or
+                      cancel your plan at any time with no commitment.
+                    </Col>
+                  </Row>
+                </Container>
+                <hr></hr>
               </div>
             </div>
           </h3>
