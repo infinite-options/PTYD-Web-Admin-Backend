918b7cdbf962367d6ae9f09640fc1b4f2c559dfc
Junha Park <jpark3@scu.edu>
2020-01-07 00:01:56 -0800

    added db connection template to server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3030;
 var mysql = require('mysql');
 
 //  Connection to database
@@ -14,7 +14,19 @@ const connection = mysql.createConnection({
 //  Initialize the app
 const app = express();
 
-//  
+//  Routing
+app.get('/', function (req, res) {
+    res.send("homepage")
+});
+
+app.get('/selectmealplan', function(req, res) {
+    connection.connect();
+    connection.query(, function (error, results, fields) {
+        if (error) throw error;
+        res.send(results)
+    });
+    connection.end();
+});
 
 //  Start the server
 app.listen(port, () => {
