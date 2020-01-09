b4e6a8d8ab142850838c710a6dc2e83f9630c271
Junha Park <jpark3@scu.edu>
2020-01-08 21:17:10 -0800

    write.py working? cannot handle double quotes in values. database stuff moved to new folder called db

--- backend/db/rds.sh
+++ backend/db/rds.sh
@@ -5,6 +5,7 @@ function usage () {
     echo "usage:";
     echo "--login, -l: log onto mysql server"
     echo "--run-schema, -r: setup schema in database, use with caution";
+    echo "--force-reset, -f: reset schema completely, including static tables";
 }
 
 function logon() {
@@ -19,6 +20,11 @@ elif [[ $# -eq 1 ]]; then
     case $1 in
     -l | --login)
         logon;
+        exit 0
+        ;;
+    -f | --force-reset)
+        mysql -h pm-mysqldb.cxjnrciilyjq.us-west-1.rds.amazonaws.com -u admin -D pricing -p < static.sql
+        mysql -h pm-mysqldb.cxjnrciilyjq.us-west-1.rds.amazonaws.com -u admin -D pricing -p < schema.sql
         exit 0
         ;;
     -r | --run-schema)
--- backend/write.py
+++ backend/write.py
@@ -17,11 +17,10 @@ else:
 def jsonifyInput():
     # Initialize JSON to return
     inputJson = {}
-#   keys = ('Subscriptions', 'Orders', 'Meals')
-    sheets_name = "122319 Test Data"
+    keys = ('Subscriptions', 'Orders', 'Meals')
+    sheets_name = "Prep To Your Door Data Schema"
     try:
-        # creds: create a client to interact with the Google Drive API
-#       scope = ['https://spreadsheets.google.com/feeds']
+        # Create a client to interact with the Google Drive API
         scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
         creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
         client = gspread.authorize(creds)
@@ -32,41 +31,17 @@ def jsonifyInput():
 
     try:
         # Find a workbook by name and open the sheets
-        subscriptions = client.open(sheets_name).get_worksheet(0)
-        orders = client.open(sheets_name).get_worksheet(1)
-#       meals = client.open(sheets_name).get_worksheet(2)
-#       paymentplans = client.open(sheets_name).get_worksheet(4)
-#       mealplans = client.open(sheets_name).get_worksheet(5)
-#       ingredients = client.open(sheets_name).get_worksheet(6)
-#       paymentadjustments = client.open(sheets_name).get_worksheet(7)
-#       for keyIndex, keyValue in enumerate(keys):
-#           print(keyIndex)
-#           records = client.open(sheets_name).get_worksheet(keyIndex)
-#           inputJson[keyValue] = records
-#       print("Retrieved data")
-#       return inputJson
-        print("Retrieved input data from Google Sheets.")
+        for key in enumerate(keys):
+            sheet = client.open(sheets_name).get_worksheet(key[0])
+            records = sheet.get_all_records()
+            if key[1] == "Orders":
+                records = consolidateMealColumns(records)
+            inputJson[key[1]] = records
+        print("Retrieved data from Google Sheets:", inputJson)
+        return inputJson
     except:
         print("Could not retrieve input data from Google Sheets.")
         raise Exception("Cannot retrieve and/or format input data")
-
-    try:
-        # Extract and format all of the values
-        orders = orders.get_all_records()
-#       meals = meals.get_all_records()
-        inputJson['Subscriptions'] = subscriptions.get_all_records()
-        inputJson['Orders'] = consolidateMealColumns(orders)
-#       inputJson['Meals'] = formatMeals(meals)
-#       inputJson['Meals'] = meals.get_all_records()
-#       inputJson['PaymentPlans'] = paymentplans.get_all_records()
-#       inputJson['MealPlans'] = mealplans.get_all_records()
-#       inputJson['Ingredients'] = ingredients.get_all_records()
-#       inputJson['PaymentAdjustments'] = paymentadjustments.get_all_records()
-        print("Formatted input data into Python dict.")
-        return inputJson
-    except:
-        print("Cannot format input data into Python dict.")
-        raise Exception("Cannot format input data")
 
 # Consolidate columns of different meal plans for the same item
 def consolidateMealColumns(orders):
@@ -75,7 +50,8 @@ def consolidateMealColumns(orders):
         rowToAppend = {}
         for key, value in order.items():
             if key.startswith('5: '):
-                newKey = formatKey(key, 3)
+#               newKey = formatKey(key, 3)
+                newKey = key[3:]
                 newValue = getMealQty(value)
                 rowToAppend[newKey] = newValue
 #               print("KEY:", key)
@@ -83,7 +59,8 @@ def consolidateMealColumns(orders):
 #               print("NEWKEY:", newKey)
 #               print("NEWVALUE (new):", "\'", newValue, "\'")
             elif any(substring in key for substring in ['10: ', '15: ', '20: ']):
-                newKey = formatKey(key, 4)
+#               newKey = formatKey(key, 4)
+                newKey = key[4:]
                 newValue = getMealQty(value)
 #               print("KEY:", key)
 #               print("VALUE:", "\'", value, "\'")
@@ -93,7 +70,8 @@ def consolidateMealColumns(orders):
 #               print("VALUETOADD:", "\'", newValue, "\'")
 #               print("NEWVALUE:", "\'", rowToAppend[newKey], "\'")
             else:
-                newKey = formatKey(key, 0)
+#               newKey = formatKey(key, 0)
+                newKey = key
                 rowToAppend[newKey] = value
 #           print("Row currently:", rowToAppend)
         newDict.append(rowToAppend)
@@ -103,10 +81,12 @@ def consolidateMealColumns(orders):
 def getMealQty(value):
     if value is '':
         return 0
-    if 'charge_type = no_charge|quantity' in value:
-        return int(value[35:])
+    if isinstance(value, str):
+        if 'charge_type = no_charge|quantity' in value:
+            return int(value[35:])
     return value
 
+'''
 # Format meals dict
 def formatMeals(meals):
     newDict = []
@@ -141,6 +121,12 @@ def replaceNullWithZero(value):
     if value is '':
         return 0
     return value
+
+# Reformat columns with name XX/XX/XXXX
+def reformatDColumn(key):
+    return "D" + key.replace('/','')
+
+'''
 
 # Returns a Connection object from pymysql to the database
 def connectToRDS(RDS_PW):
@@ -188,14 +174,17 @@ def dictToSql(jsonDicts, tableName, tableInfo):
                 if '/' in key:
                     continue
 #                   key = reformatDColumn(key)
+#               print("Finished slash in key check")
                 if 'phone' in key.lower():
                     value = reformatPhoneNum(value)
+#               print("Finished phone in key check")
+#               print(tableInfo)
                 valueType = tableInfo[key]
+#               print("valueType:", valueType)
                 if value == '':
                     value = 'NULL'
                     valueType = 'null_value'
                 ins = appendSqlItem(ins, key, "column_name")
-#               print("valueType:", valueType)
                 val = appendSqlItem(val, value, valueType)
 #               print("Current ins:", ins)
 #               print("Current val:", val)
@@ -227,9 +216,6 @@ def reformatPhoneNum(num):
 #   print("Final value:", formatNum)
     return formatNum
 
-# Reformat columns with name XX/XX/XXXX
-def reformatDColumn(key):
-    return "D" + key.replace('/','')
 
 # Reformate date values from input data
 def formatInputDate(inputDate):
@@ -241,12 +227,15 @@ def formatInputDate(inputDate):
 def appendSqlItem(cmd, item, data_type):
     if any(nonStringType in data_type for nonStringType in ['int', 'column_name', 'null_value']):
         item = str(item)
+        if data_type == 'column_name':
+            item = "`" + str(item) + "`"
     else:
         # Use the below two lines if Date_Submitted value format is Dec 9, 2019, 17:00:00
-#       if data_type == 'datetime':
-#           item = formatInputDate(str(item))
+        if data_type == 'datetime':
+            item = formatInputDate(str(item))
         item = "\'" + str(item) + "\'"
     newCmd = cmd + item
+#   print("appendSqlItem():", newCmd)
     return newCmd + ", "
 
 # Close an SQL command
