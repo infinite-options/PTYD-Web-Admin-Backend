11c65fd3dacb0a6a7b993541add638e860eaae8b
Strother Woog <strowoog@gmail.com>
2020-01-09 14:20:08 -0800

    dropdown on navbar

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Nav from "react-bootstrap/Nav";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
@@ -51,13 +52,16 @@ class App extends Component {
               </div>
 
               <Nav.Item Style="margin-top:90px">
-
-                <Nav.Link Style="color:black" href="/selectmealplan">SUBSCRIBE</Nav.Link>
+                <Nav.Link Style="color:black" href="/selectmealplan">
+                  SUBSCRIBE
+                  </Nav.Link>
               </Nav.Item>
-              <Nav.Item Style="margin-top:90px">
-                <Nav.Link Style="color:black" href="/menuthisweek">MENU</Nav.Link>
+              
+              <NavDropdown title="MENU" id="nav-dropdown" Style="margin-top:90px; color:black;">
+                <NavDropdown.Item href="/menuthisweek">THIS WEEK</NavDropdown.Item>
+                <NavDropdown.Item href="/menunextweek">NEXT WEEK</NavDropdown.Item>
+              </NavDropdown>
 
-              </Nav.Item>
               <Nav.Item Style="margin-top:90px">
                 <Nav.Link Style="color:black" eventKey="link-2">
                   FIND US
@@ -202,7 +206,7 @@ class App extends Component {
                   </Nav.Link>
                 </Nav>
                 <Nav defaultActiveKey="/">
-                  <a href="/">
+                  <a href="https://www.facebook.com/preptoyourdoor" target="_blank">
                     <img
                       id="i3fogh650imageimageimage"
                       alt="Grey Facebook Icon"
@@ -212,7 +216,7 @@ class App extends Component {
                       src="https://static.wixstatic.com/media/d3470ec8ca26475da4b228f0199b5d3d.png/v1/fill/w_51,h_51,al_c,q_95/d3470ec8ca26475da4b228f0199b5d3d.webp"
                     ></img>
                   </a>
-                  <a href="/">
+                  <a href="https://twitter.com/preptoyourdoor" target="_blank">
                     <img
                       id="i3fogh651imageimageimage"
                       alt="Grey Twitter Icon"
@@ -222,7 +226,7 @@ class App extends Component {
                       src="https://static.wixstatic.com/media/7177d158c36d432b93f51e54f80e2f3c.png/v1/fill/w_51,h_51,al_c,q_95/7177d158c36d432b93f51e54f80e2f3c.webp"
                     ></img>
                   </a>
-                  <a href="/">
+                  <a href="https://www.instagram.com/preptoyourdoor/" target="_blank">
                     <img
                       id="i3fogh652imageimageimage"
                       alt="Grey Instagram Icon"
