32305b580eb320839288ec75d03f59e16e705534
Tangni Wang <twang63@ucsc.edu>
2020-02-09 13:46:21 -0800

    an attempt for modal select meal

--- client/src/components/meal-button.js
+++ client/src/components/meal-button.js
@@ -26,9 +26,20 @@ export default class MealButton extends Component {
     requestModal: false,
     countFood: 0,
     buttonDisabled: true,
-    buttonSelectKeepColor: false
+    buttonSelectKeepColor: false,
+    modalShowSelectMeal: false
   };
 
+  setModalShowModal = () => {
+    this.setState({
+      modalShowSelectMeal: true
+    });
+  };
+  turnOffModalShow = () => {
+    this.setState({
+      modalShowSelectMeal: false
+    });
+  };
   closeButtonSelect = () => {
     this.setState({
       buttonSelect: false,
@@ -164,8 +175,38 @@ export default class MealButton extends Component {
         </ButtonToolbar>
       );
     }
+
+
+
     return (
       <div>
+            function SelectMealModal(props) {
+      return (
+        <Modal
+          {...props}
+          size="lg"
+          aria-labelledby="contained-modal-title-vcenter"
+          centered
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+              Special Request
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+              <Form.Control as="textarea" rows="3" />
+            </Form.Group>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+            <Button onClick={props.onHide}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      );
+    }
         <ButtonToolbar>
           <div className="radio">
             <Button
@@ -228,47 +269,65 @@ export default class MealButton extends Component {
           &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
           <SpecialRequestAnimation />
           <div style={this.state.buttonSelect ? {} : { display: "none" }}>
-            {this.SelectMealEachMeal()}
+            {this.SelectMealAnimation}
           </div>
         </ButtonToolbar>
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
       </div>
     );
   }
-
-  SelectMealEachMeal = () => {
+  SelectMealAnimation = () => {
     return (
-      <Modal.Dialog>
-        <Modal.Header>
-          <center>
-            <Modal.Title>SEASONAL FAVORITES</Modal.Title>
-          </center>
-        </Modal.Header>
-        <Modal.Body>
-          <Grid>
-            <Cell col={6}>
-              <EachMeal
-                mealTitle="WHITE BEAN KALE SOUP"
-                ingridents="Ingredients: White Beans, Kale, Vegetable Broth"
-                detail="Cal 500, Prot 27, Carb 41, Sug 5, Fib 9, Fat 29, Sat 5"
-              />
-            </Cell>
-            <Cell col={6}>
-              <EachMeal
-                mealTitle="WHITE BEAN KALE SOUP"
-                ingridents="Ingredients: White Beans, Kale, Vegetable Broth"
-                detail="Cal 500, Prot 27, Carb 41, Sug 5, Fib 9, Fat 29, Sat 5"
-              />
-            </Cell>
-          </Grid>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={this.closeButtonSelect}>
-            Close
-          </Button>
-          <Button variant="primary">Save changes</Button>
-        </Modal.Footer>
-      </Modal.Dialog>
+      <ButtonToolbar>
+        <Button
+          variant="outline-dark"
+          style={{ width: "90px", height: "90px" }}
+          onClick={this.setModalShowModal}
+        >
+          Special Requ
+        </Button>
+
+        <SelectMealModal
+          show={this.state.modalShowSelectMeal}
+          onHide={this.turnOffModalShow}
+        />
+      </ButtonToolbar>
     );
   };
+
+  // SelectMealEachMeal = () => {
+  //   return (
+  //     <Modal.Dialog>
+  //       <Modal.Header>
+  //         <center>
+  //           <Modal.Title>SEASONAL FAVORITES</Modal.Title>
+  //         </center>
+  //       </Modal.Header>
+  //       <Modal.Body>
+  //         <Grid>
+  //           <Cell col={6}>
+  //             <EachMeal
+  //               mealTitle="WHITE BEAN KALE SOUP"
+  //               ingridents="Ingredients: White Beans, Kale, Vegetable Broth"
+  //               detail="Cal 500, Prot 27, Carb 41, Sug 5, Fib 9, Fat 29, Sat 5"
+  //             />
+  //           </Cell>
+  //           <Cell col={6}>
+  //             <EachMeal
+  //               mealTitle="WHITE BEAN KALE SOUP"
+  //               ingridents="Ingredients: White Beans, Kale, Vegetable Broth"
+  //               detail="Cal 500, Prot 27, Carb 41, Sug 5, Fib 9, Fat 29, Sat 5"
+  //             />
+  //           </Cell>
+  //         </Grid>
+  //       </Modal.Body>
+  //       <Modal.Footer>
+  //         <Button variant="secondary" onClick={this.closeButtonSelect}>
+  //           Close
+  //         </Button>
+  //         <Button variant="primary">Save changes</Button>
+  //       </Modal.Footer>
+  //     </Modal.Dialog>
+  //   );
+  // };
 }
