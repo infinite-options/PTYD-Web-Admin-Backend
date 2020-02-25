24c353380d41b69c8ec3a7c8007144665e90012b
Tangni Wang <twang63@ucsc.edu>
2020-02-25 12:10:46 -0800

    added react dom route and landing page

---
+++ admin/src/components/landingpage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+class LandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>admin Page</h1>
+      </div>
+    );
+  }
+}
+export default LandingPage;
---
+++ admin/src/components/main.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import LandingPage from "./landingpage";
+
+const Main = () => (
+  <Switch>
+    <Route exact path="/" component={LandingPage} />
+  </Switch>
+);
+export default Main;
