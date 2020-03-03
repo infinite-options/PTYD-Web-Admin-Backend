a29c582d7e7d462349a8ffcb788488b339c34dfb
Justin Ling <48165446+justinrling@users.noreply.github.com>
2020-03-02 18:15:01 -0800

    Admin Page Updates 3/1/20
    First pass at creating a layout for the PTYD Admin Page

---
+++ admin/src/components/NavigationBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Nav, Navbar } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const Styles = styled.div`
+    navbar {
+        background-color: #222;
+    }
+
+    .navbar-brand .navbar-nav .nav-link {
+        color: #bbb;
+
+        &:hover {
+            color: withTheme;
+        }
+    }
+`;
+
+export const NavigationBar = () => (
+    <Styles>
+        <Navbar expand="lg">
+            <Navbar.Brand href="/">Prep To Your Door</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ml-auto">
+                    <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+                    <Nav.Item><Nav.Link href="/contact">Contact</Nav.Link></Nav.Item>                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </Styles>
+)
--- admin/src/components/main.js
+++ admin/src/components/main.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { Switch, Route } from "react-router-dom";
-import LandingPage from "./landingpage";
+// import React from "react";
+// import { Switch, Route } from "react-router-dom";
+// import LandingPage from "./Home";
 
-const Main = () => (
-  <Switch>
-    <Route exact path="/" component={LandingPage} />
-  </Switch>
-);
-export default Main;
+// const Main = () => (
+//   <Switch>
+//     <Route exact path="/" component={LandingPage} />
+//   </Switch>
+// );
+// export default Main;
