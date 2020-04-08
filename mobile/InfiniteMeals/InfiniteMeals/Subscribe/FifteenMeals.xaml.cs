14a102da2ec2af7439c998832cec7d9ff5e86d52
Landen Lefrancois <llefranc@ucsc.edu>
2020-04-08 13:16:16 -0700

    resubmit

---
+++ mobile/InfiniteMeals/InfiniteMeals/Subscribe/FifteenMeals.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InfiniteMeals.Subscribe
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FifteenMeals : ContentPage
+    {
+        public FifteenMeals()
+        {
+            InitializeComponent();
+
+            mealimage.Source = ImageSource.FromFile("mealplan.jpg");
+            mealimage1.Source = ImageSource.FromFile("mealplan.jpg");
+            mealimage2.Source = ImageSource.FromFile("mealplan.jpg");
+        }
+    }
+}
---
+++ mobile/InfiniteMeals/InfiniteMeals/User/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InfiniteMeals.User
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User() { }
+        public User(String Username, string Password)
+        {
+            this.Username = Username;
+
+            this.Password = Password;
+        }
+
+        public bool Authenticate(String Username, String Password)
+        {
+            if(this.Username.Equals(Username) && this.Password.Equals(Password)){
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
