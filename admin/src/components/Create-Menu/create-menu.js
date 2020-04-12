e86a7df9a46da8a62d7839cc22f816aac9bdf6c9
Tangni Wang <twang63@ucsc.edu>
2020-04-11 22:04:10 -0700

    update avg to be saved with meal selection

--- admin/src/components/Create-Menu/create-menu.js
+++ admin/src/components/Create-Menu/create-menu.js
@@ -15,8 +15,16 @@ class CreateMenu extends Component {
       datekeys: [],
       details: [],
       selection: 0,
-      selection2: 0,
-      showAdd: false
+      showAdd: false,
+      selectionOfDropMenu: [] //remembering Meal selection dropdown
+
+      /**
+       * selectionOfDropMenu[0] = 8
+       * selectionOfDropMenu[1] = 0
+       * selectionOfDropMenu[2] = 0
+       * selectionOfDropMenu[3] = 0
+       *
+       */
     };
   }
   async componentWillMount() {
@@ -24,40 +32,89 @@ class CreateMenu extends Component {
     const api = await res.json();
     const createMenu = api.menus;
     const avg = api.result;
-    let tempkeys = [];
 
+    let tempkeys = [];
+    let mealMap = {};
+    for (let x = 0; x < avg.length; x++) {
+      mealMap[avg[x].Meal_Name] = x; //maps meal to number
+      mealMap[x] = avg[x].Meal_Name;
+    }
+    console.log("tanny says ", mealMap);
     for (var key of Object.keys(createMenu)) {
       tempkeys.push(key);
     }
     // tempkeys = tempkeys.reverse();
-    this.setState({ datekeys: tempkeys, createMenu, avg });
+    let x = tempkeys[0];
+    let len = createMenu[x].length;
+    let tempSelectionOfDropMenu = new Array(len).fill(0);
+    let initArr = createMenu[x];
+    for (let i = 0; i < len; i++) {
+      tempSelectionOfDropMenu[i] = mealMap[initArr[i]["Meal_Name"]];
+    }
+
+    this.setState(
+      {
+        selectionOfDropMenu: tempSelectionOfDropMenu,
+        datekeys: tempkeys,
+        createMenu,
+        avg,
+        mealMap
+      },
+      () => {
+        console.log("selection of drops ", this.state.selectionOfDropMenu);
+      }
+    );
   }
+  //date
   handleChange = event => {
+    //get new dropdown value
+    let x = this.state.datekeys[event.target.value];
+    let len = this.state.createMenu[x].length;
+    let tempSelectionOfDropMenu = new Array(len).fill(0);
+    let initArr = this.state.createMenu[x];
+    for (let i = 0; i < len; i++) {
+      tempSelectionOfDropMenu[i] = this.state.mealMap[initArr[i]["Meal_Name"]];
+    }
+
     this.setState({
-      selection: event.target.value
+      selection: event.target.value,
+      selectionOfDropMenu: tempSelectionOfDropMenu
     });
   };
-  handleChange2 = event => {
+
+  //single dropdown for meal
+  handleChange2 = (newValue, index) => {
+    let arr = this.state.selectionOfDropMenu;
+    arr[index] = newValue;
+    let day = this.state.datekeys[this.state.selection]; //get the current date
+    let newCreateMenu = this.state.createMenu;
+    let mealArray = newCreateMenu[day];
+    mealArray[index].Meal_Name = this.state.mealMap[newValue];
+
     this.setState({
-      selection2: event.target.value
+      selectionOfDropMenu: arr,
+      CreateMenu: newCreateMenu
     });
   };
+
   render() {
     let displayrows = [];
     let enterDate = this.state.datekeys[this.state.selection];
     if (enterDate == null) {
       return <div></div>;
     }
+
     let arr = this.state.createMenu[enterDate];
     // arr = arr.reverse();
     if (arr == null) {
       return <div></div>;
     }
+
     for (let i = 0; i < arr.length; i++) {
       let tempelement = (
         <tr>
           <td>{arr[i].Menu_Type}</td>
-          <td>{this.mealDropdown(arr[i].Meal_Name)}</td>
+          <td>{this.mealDropdown(arr[i].Meal_Name, i)}</td>
           <td>{this.avgpost(arr[i].Meal_Name)}</td>
         </tr>
       );
@@ -207,31 +264,54 @@ class CreateMenu extends Component {
       </FormControl>
     );
   };
+
   avgpost = mealName => {
     for (let i = 0; i < this.state.avg.length; i++) {
+      console.log(mealName, " and ", this.state.avg[i]["Meal_Name"]);
+
       if (this.state.avg[i]["Meal_Name"] == mealName) {
+        console.log("matched ", this.state.avg[i]["Avg_Sales_Posting"]);
+
         return this.state.avg[i]["Avg_Sales_Posting"];
       }
     }
     return "N/A";
   };
-  mealDropdown = mealDefault => {
+
+  mealDropdown = (mealDefault, index) => {
     let tempmeal = [];
     for (let i = 0; i < this.state.avg.length; i++) {
-      tempmeal.push(
-        <MenuItem value={i}>{this.state.avg[i]["Meal_Name"]}</MenuItem>
+      console.log(
+        "key: ",
+        this.state.mealMap[this.state.avg[i]["Meal_Name"]],
+        " meal : ",
+        this.state.avg[i]["Meal_Name"]
       );
-      // if (this.state.avg[i]["Meal_Name"] == mealDefault) {
-      //   return this.state.avg[i]["Avg_Sales_Posting"];
-      // }
+      tempmeal.push(
+        <MenuItem value={this.state.mealMap[this.state.avg[i]["Meal_Name"]]}>
+          {this.state.avg[i]["Meal_Name"]}
+        </MenuItem>
+      );
     }
+
+    console.log(
+      " item is ",
+      this.state.mealMap[this.state.avg[index]["Meal_Name"]],
+      " with ",
+      this.state.avg[index]["Meal_Name"],
+      " realValue : ",
+      mealDefault
+    );
+
     return (
       <FormControl>
         <Select
           labelId="demo-simple-select-label"
           id="demo-simple-select"
-          value={this.state.selection2}
-          onChange={this.handleChange2}
+          value={this.state.selectionOfDropMenu[index]}
+          onChange={e => {
+            this.handleChange2(e.target.value, index);
+          }}
           // style={{ color: "white" }}
         >
           {tempmeal}
