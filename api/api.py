432b0d490cbbbb5c0f7b2e1ca24f078ba9a3f6f5
Junha Park <jpark3@scu.edu>
2020-04-23 21:24:44 -0700

    Removed unneeded commented localhost RDS info

--- api/api.py
+++ api/api.py
@@ -16,9 +16,7 @@ import json
 import pymysql
 
 RDS_HOST = 'pm-mysqldb.cxjnrciilyjq.us-west-1.rds.amazonaws.com'
-#RDS_HOST = 'localhost'
 RDS_PORT = 3306
-#RDS_USER = 'root'
 RDS_USER = 'admin'
 RDS_DB = 'ptyd'
 
