a84b8ef56452da4124cabe1606a8cf785ecf55e8
pmarathay <pmarathay@infiniteoptions.com>
2020-05-10 13:23:52 -0700

    Changed private key file name

--- snapshots/connect-to-ec2.sh
+++ snapshots/connect-to-ec2.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 BASE_DIR="$(cd "$(dirname "$0" )" && pwd )"
-EC2_KEY_DIR=$BASE_DIR"/aws-ec2-ptyd-admin.pem"
-EC2_ENDPOINT="ec2-user@ec2-18-236-97-142.us-west-2.compute.amazonaws.com"
+
+# ptyd-0510.pem is the private key file needed to access EC2 (key pair file)
+EC2_KEY_DIR=$BASE_DIR"/ptyd-0510.pem"
+
+# ec2-54-183-61-181.us-west-1.compute.amazonaws.com from Public DNS (IPv4)
+EC2_ENDPOINT="ec2-user@ec2-54-183-61-181.us-west-1.compute.amazonaws.com"
 
 ssh -i $EC2_KEY_DIR $EC2_ENDPOINT
