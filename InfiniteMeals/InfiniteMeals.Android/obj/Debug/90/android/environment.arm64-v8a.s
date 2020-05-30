f1781b3452298f6862590434032bdd8b9a91a6d3
squach28 <squach3443@gmail.com>
2020-05-30 12:38:31 -0700

    database fetch implemented + comments for db.cs

--- InfiniteMeals/InfiniteMeals.Android/obj/Debug/90/android/environment.arm64-v8a.s
+++ InfiniteMeals/InfiniteMeals.Android/obj/Debug/90/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"b632885a-df5c-4845-96da-6035a4abcd71"
+	.asciz	"94c751ca-f222-49a3-aaad-e86bf3486aed"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- InfiniteMeals/InfiniteMeals/ViewModel/MealSelect/MealSchedule.xaml.cs
+++ InfiniteMeals/InfiniteMeals/ViewModel/MealSelect/MealSchedule.xaml.cs
@@ -1,102 +1,102 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-namespace InfiniteMeals.Meals
-{
+using InfiniteMeals.Model.Database;
+
+namespace InfiniteMeals.Meals {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MealSchedule : ContentPage
-    {
+    public partial class MealSchedule : ContentPage {
         int ctr = 2;
         bool disabled = false;
         List<String> order = new List<String>();
 
         String green = "#8FBC8F";
 
-        public MealSchedule()
-        {
+        public MealSchedule() {
             InitializeComponent();
+            getData();
         }
 
-        private async void ClickedSelectMeal(object sender, EventArgs e)
-        {
-            if(ctr == 2 | disabled == true)
-            await Navigation.PushAsync(new MealSelect.MealChoices());
-            else
-            {
+        private async void getData() {
+    
+
+            HttpClient client = new HttpClient();
+            var content = await client.GetStringAsync("https://uavi7wugua.execute-api.us-west-1.amazonaws.com/dev/api/v2/meals");
+            var obj = JsonConvert.DeserializeObject<Data>(content); // access database info from obj
+            System.Diagnostics.Debug.WriteLine("test");
+            System.Diagnostics.Debug.WriteLine(obj.Message);
+            System.Diagnostics.Debug.WriteLine(obj.Result.MenuForWeek1.Meals.Weekly.Menu[0].MealName);
+        
+
+        }
+
+        private async void ClickedSelectMeal(object sender, EventArgs e) {
+            if (ctr == 2 | disabled == true)
+                await Navigation.PushAsync(new MealSelect.MealChoices());
+            else {
                 // Toast to user
             }
         }
 
-        private void ClickedColor(object sender, EventArgs e)
-        {
+        private void ClickedColor(object sender, EventArgs e) {
             Button btn = (Button)sender;
             System.Diagnostics.Debug.WriteLine(btn.ClassId + " hi " + disabled);
             // Change button colors
-            if (btn.ClassId == "SkipButton")
-            {
-                    if (btn.BackgroundColor.Equals(Color.FromHex(green)))
-                    {
-                        btn.BackgroundColor = Color.Default;
-                        disabled = false;
-                        ctr = 0;
-                    }
-                    else if (btn.BackgroundColor.Equals(Color.Default))
-                    {
-                        btn.BackgroundColor = Color.FromHex(green);
+            if (btn.ClassId == "SkipButton") {
+                if (btn.BackgroundColor.Equals(Color.FromHex(green))) {
+                    btn.BackgroundColor = Color.Default;
+                    disabled = false;
+                    ctr = 0;
+                }
+                else if (btn.BackgroundColor.Equals(Color.Default)) {
+                    btn.BackgroundColor = Color.FromHex(green);
 
-                        disabled = true;
-                        ctr = 2;
-                    }
+                    disabled = true;
+                    ctr = 2;
+                }
             }
-            else
-            {
-                if(disabled == false)
-                {
-                    if (btn.BackgroundColor.Equals(Color.Default))
-                    {
-                        if (ctr < 2)
-                        {
+            else {
+                if (disabled == false) {
+                    if (btn.BackgroundColor.Equals(Color.Default)) {
+                        if (ctr < 2) {
                             btn.BackgroundColor = Color.FromHex(green);
                             order.Add(btn.Text);
                             ctr++;
                         }
                     }
-                    else
-                    {
+                    else {
                         btn.BackgroundColor = Color.Default;
                         order.Remove(btn.Text);
                         ctr--;
                     }
                 }
             }
-              
+
             System.Diagnostics.Debug.WriteLine(btn.ClassId + " bye " + disabled);
         }
 
-        private void ClickedAddOn(object sender, EventArgs e)
-        {
+        private void ClickedAddOn(object sender, EventArgs e) {
             // Change button colors
 
             Button btn = (Button)sender;
 
-            if (btn.BackgroundColor.Equals(Color.Default))
-            {
-                    btn.BackgroundColor = Color.FromHex("#F9E29C");
+            if (btn.BackgroundColor.Equals(Color.Default)) {
+                btn.BackgroundColor = Color.FromHex("#F9E29C");
             }
-            else
-            {
+            else {
                 btn.BackgroundColor = Color.Default;
             }
         }
 
-        private async void ClickedStepper(object sender, EventArgs e)
-        {
+        private async void ClickedStepper(object sender, EventArgs e) {
             await Navigation.PushAsync(new Steppers.BasicSteppers());
         }
 
