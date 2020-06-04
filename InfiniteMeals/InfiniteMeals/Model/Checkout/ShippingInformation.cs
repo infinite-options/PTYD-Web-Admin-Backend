c3c5eba6608a8ef87a551f10c756d49031c39459
squach28 <squach3443@gmail.com>
2020-06-04 16:56:05 -0700

    address line 2 added to shipping + payment page UI

---
+++ InfiniteMeals/InfiniteMeals/Model/Checkout/ShippingInformation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using System.ComponentModel;
+
+namespace InfiniteMeals.Model.Checkout {
+    class ShippingInformation: INotifyPropertyChanged {
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public string firstName { get; set; } 
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string addressOne { get; set; }
+        public string addressTwo { get; set; }
+        public string zipCode { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+
+        public ShippingInformation() {
+            this.firstName = "";
+            this.lastName = "";
+            this.phoneNumber = "";
+            this.addressOne = "";
+            this.addressTwo = "";
+            this.zipCode = "";
+            this.city = "";
+            this.state = "";
+        }
+
+        public ShippingInformation(string firstName, string lastName, 
+            string phoneNumber, string addressOne, string addressTwo, string zipCode, string city, string state) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.phoneNumber = phoneNumber;
+            this.addressOne = addressOne;
+            this.addressTwo = addressTwo;
+            this.zipCode = zipCode;
+            this.city = city;
+            this.state = state;
+        }
+    }
+}
--- InfiniteMeals/InfiniteMeals/obj/Debug/netstandard2.0/ViewModel/Checkout/Payment.xaml.g.cs
+++ InfiniteMeals/InfiniteMeals/obj/Debug/netstandard2.0/ViewModel/Checkout/Payment.xaml.g.cs
@@ -17,8 +17,24 @@ namespace InfiniteMeals.ViewModel.Checkout {
     public partial class Payment : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry cardNumberNameEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry cardHolderNameEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry expirationDateEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
+        private global::Xamarin.Forms.Entry cvvEntry;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "2.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(Payment));
+            cardNumberNameEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "cardNumberNameEntry");
+            cardHolderNameEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "cardHolderNameEntry");
+            expirationDateEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "expirationDateEntry");
+            cvvEntry = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Entry>(this, "cvvEntry");
         }
     }
 }
