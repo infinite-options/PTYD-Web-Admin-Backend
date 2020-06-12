23a3f7f9bef4f19a67475c469430892f0cc7db60
aqxin <aqxin.cs@gmail.com>
2020-06-11 23:11:28 -0700

    Move QuoteCarousel.js inline CSS to App.css

--- client/src/components/QuoteCarousel.js
+++ client/src/components/QuoteCarousel.js
@@ -27,8 +27,8 @@ export default class QuoteCarousel extends Component {
           <Slider {...settings}
           >
             <div>
-                <div style={{padding: "0 200px"}}>
-                    <h3 className='font1' style={{lineHeight: "50px"}}>
+                <div className='quote-carousel-padding'>
+                    <h3 className='font1 quote-line-height'>
                     "PTYD is so good and we love it so much, it feels like part of
                     our family! It's so thrilling to fill our fridge every week
                     with tasty surprises!"
@@ -37,19 +37,19 @@ export default class QuoteCarousel extends Component {
             </div>
 
             <div>
-                <div style={{padding: "0 140px"}}>
-                    <h3 className='font1' style={{lineHeight: "50px", marginBottom:"0", paddingBottom:"0"}}>
-                    "As someone who loves to relax on the weekend...</h3>
-                    <h3 className='font1' style={{lineHeight: "50px", marginTop:"0", paddingTop:"0"}}>
-                    I am so thankful that I found Prep."
+                <div className='quote-carousel-padding2'>
+                    <h3 id='quote2' className='font1'>
+                        "As someone who loves to relax on the weekend...</h3>
+                    <h3 id='quote2' className='font1'>
+                        I am so thankful that I found Prep."
                     </h3>
                 </div>
             </div>
 
             <div>
-                <div style={{padding: "0 200px"}}>
-                    <h3 className='font1' style={{lineHeight: "50px"}}>
-                    "A passionate company that offers healthy, nutritionist-designed meals that are as convenient as they are delicious."
+                <div className='quote-carousel-padding'>
+                    <h3 className='font1 quote-line-height'>
+                        "A passionate company that offers healthy, nutritionist-designed meals that are as convenient as they are delicious."
                     </h3>
                 </div>
             </div>
