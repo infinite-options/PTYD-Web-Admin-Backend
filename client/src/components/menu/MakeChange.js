87eb8b2e62a9ec25360a28d8d27df787a5770c03
quangdang87 <vinhquangdang1@gmail.com>
2020-06-15 16:41:40 -0700

    put time in date format

--- client/src/components/menu/MakeChange.js
+++ client/src/components/menu/MakeChange.js
@@ -196,7 +196,7 @@ export default class MakeChange extends Component {
         //update changing payment for subcription
         let creditCard = this.state.creditCard;
         let date = moment(
-          `${creditCard.cc_exp_year}-${creditCard.cc_exp_month}-01`
+          `${creditCard.cc_exp_year}-${creditCard.cc_exp_month}-01 00:00:00`
         ).format("YYYY-MM-DD");
         await axios.patch(this.props.UPDATE_PAYMENT_URL, {
           purchase_id: this.state.currentPurchase.purchase_id,
