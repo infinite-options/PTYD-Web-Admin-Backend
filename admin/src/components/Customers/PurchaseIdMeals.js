563cd2bc2282db75218a8b28b09c0324edc84e6f
Lyman Shen <lyman.shen@gmail.com>
2020-07-26 11:39:15 -0700

    Reset attempts to change column width, saved in file outside Git

--- admin/src/components/Customers/PurchaseIdMeals.js
+++ admin/src/components/Customers/PurchaseIdMeals.js
@@ -69,68 +69,18 @@ class PurchaseIdMeals extends React.Component {
                     columns={[
                         {   title: 'Delivery First Name',
                             field: 'delivery_first_name',
-                            cellStyle: {
-                                width: 60,
-                                maxWidth: 60,
-                                padding: 0,
-                                margin: 0,
-                                // overflowWrap: 'break-word',
-                            },
-                            headerStyle: {
-                                width: 60,
-                                maxWidth: 60,
-                                padding: 0,
-                                margin: 0,
-                            }
                         },
                         {   title: 'Delivery Last Name',
                             field: 'delivery_last_name',
-                            // cellStyle: {
-                            //     backgroundColor: 'blue',
-                            //     width: 90,
-                            //     maxWidth: 90,
-                            //     overflowWrap: 'break-word',
-                            // },
-                            headerStyle: {
-                                width: 90,
-                                maxWidth: 90,
-                                backgroundColor: 'blue',
-                            },
-                            render: row => (
-                                (<div style={{
-                                    width: '90px',
-                                    backgroundColor: 'blue',
-                                    // overflowWrap: 'break-word',
-                                }}>
-                                    {row.delivery_last_name}
-                                </div>)
-                            )
                         },
                         {   title: 'Delivery Phone',
                             field: 'delivery_phone',
-                            cellStyle: {
-                                width: 100,
-                                maxWidth: 100,
-                                overflowWrap: 'break-word',
-                            },
-                            headerStyle: {
-                                width: 100,
-                                maxWidth: 100,
-                            }
                         },
                         { title: 'Delivery Address', field: 'delivery_address'},
                         { title: 'Delivery Address Unit', field: 'delivery_address_unit'},
                         { title: 'Delivery City', field: 'delivery_city'},
                         {   title: 'Delivery ZIP',
                             field: 'delivery_zip',
-                            cellStyle: {
-                                backgroundColor: 'aqua',
-                                // maxWidth:'75px',
-                                // overflowWrap: 'break-word',
-                            },
-                            headerStyle: {
-                                // maxWidth: '75px'
-                            }
                         },
                         { title: 'Meal Plan Description', field: 'meal_plan_desc'},
                         { title: 'Saturday', field: 'Saturday' },
